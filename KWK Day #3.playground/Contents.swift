9dd5a9cb5028739aefb6bdace7ce6d8e992922e3
Apple <apple@Apples-MacBook-Pro-34.local>
2019-08-08 12:07:52 -0500

    2 Storyboards

---
+++ KWK Day #3.playground/Contents.swift
@@ -0,0 +1,65 @@
+//class Dinosaur {
+//    var age = 0
+//    var breed = ""
+//    var color = "lavender"
+//    init (dinosaurBreed : String,
+//          dinosaurAge : Int){
+//        breed = dinosaurBreed
+//        age = dinosaurAge
+//    }
+//    func eatFood() {
+//        print("The \(color) \(breed) is \(age) and likes to eat food!")
+//    }
+//}
+//var newDinosaur = Dinosaur(dinosaurBreed : "T-Rex", dinosaurAge : 5 )
+//newDinosaur.eatFood()
+
+//class Dog {
+//    var favoriteBreeds = ["Labrador",
+//                          "Pug",
+//                          "Pomeranian"]
+//    var name = ""
+//    var age = 0
+//    var breed = ""
+//    init (dogName : String,
+//          dogAge : Int,
+//          dogBreed : String ){
+//    name = dogName
+//    age = dogAge
+//        breed = dogBreed }
+//    func makeBark() {
+//        print ("I'm \(name), woof woof")
+//
+//
+//    for breeds in favoriteBreeds {
+//    print("My favorite dog breed is \(breeds)")}
+//    }
+//
+//    }
+//
+//var newDog = Dog(dogName : "Baileigh", dogAge: 7 , dogBreed: "Labrador")
+//newDog.makeBark()
+//
+
+//var name = "Robin"
+//print("i am \(name)")
+//print("Happy Birthday \(name)")
+
+////"karlie" == "karlie"
+////"karlie" == " karlie"
+////"Karlie" == "karlie"
+//var luckyNumber = 7
+//luckyNumber > 3
+//print("Robin is \(luckyNumber) years old")
+
+
+//var dogAge = 5
+//if dogAge < 2 {
+//    print("You are a puppy 🐶")
+//}
+//else if dogAge > 12 {
+//    print("You are elderly 💗")
+//}
+//else {
+//    print("You are also awesome 🐾")
+//}
---
+++ KWK Day #4.playground/Contents.swift
@@ -0,0 +1,47 @@
+////var number = 0, 1 ... 100
+////var number = Int
+////if number = (3,6,9,12,15,18)
+////    print("Fizz")
+////
+////var number/3
+////
+////
+////
+////print(1,
+////      2,
+////      3,
+////      4,
+////      5,
+////      6,
+////      7,
+////      100)
+////
+////for i in 1...100 {
+////
+////    if i % 3 == 0 && i % 5 == 0 {
+////        print("FizzBuzz")
+////    }
+////    else if i % 3 == 0 {
+////        print("Fizz")
+////    }
+////    else if i % 5 == 0 {
+////        print("Buzz")
+////    }
+////    else {
+////        print(i)
+////    }
+////}
+//
+//for i in 1...100 {
+//
+//    if i % 3 == 0 {
+//        print("Fizz")
+//    }
+//        else if i % 5 == 0 {
+//            print("Buzz")
+//        }
+//        else {
+//            print(i)
+//        }
+//    }
+//}
