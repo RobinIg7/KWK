2a70d20f3ec86546cdf0bbe23b9c7790b61f1e13
Apple <apple@Apples-MacBook-Pro-34.local>
2019-08-12 11:07:16 -0500

    Complete Iteration 0

---
+++ Robin Igwe FP/Robin Igwe FP/ToDo.swift
@@ -0,0 +1,21 @@
+//
+//  ToDo.swift
+//  Robin Igwe FP
+//
+//  Created by Apple on 8/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {//class brackets
+    var name = " "
+    var important = false
+    
+    
+    init(ToDoname : String, ToDoimportant : Bool) {
+        name = ToDoname
+        important = ToDoimportant
+    }
+}//class brackets
+
